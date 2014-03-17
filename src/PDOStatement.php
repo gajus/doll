a929ddc1705d40d0a27cbef48c1fe11817aa1178
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-03-17 06:22:59 +0000

    Handling the HY093 error that is not trigger Exception.

--- src/PDOStatement.php
+++ src/PDOStatement.php
@@ -26,9 +26,22 @@ class PDOStatement extends \PDOStatement {
 	 */
 	public function execute ($parameters = []) {
 		if ($parameters) {
-			parent::execute($parameters);
+			$execute = parent::execute($parameters);
 		} else {
-			parent::execute();
+			$execute = parent::execute();
+		}
+
+		if ($execute === false) {
+			$error = $this->errorInfo();
+
+			if ($error[0] === 'HY093') {
+				// For some odd reason PDO does no throw Exception in this case.
+				// @todo Cover with unit tests.
+				// @see http://www.php.net/manual/en/pdostatement.execute.php
+				throw new Exception\InvalidArgumentException('You cannot bind multiple values to a single parameter. You cannot bind more values than specified.');
+			} else {
+				throw new Exception\RuntimeException('Oops. Something gone terribly wrong.');
+			}
 		}
 
 		$this->dbh->on('execute', $this->queryString, $parameters);
